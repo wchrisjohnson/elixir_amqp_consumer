03cde5155677f60dfe52ea8fa4d97469b7e9ce1b
Chris Johnson <wchrisjohnson@gmail.com>
2013-07-27 17:24:16 -0400

    thanks to Jose Valim, the start now works!

--- lib/elixir_amqp_consumer/server.ex
+++ lib/elixir_amqp_consumer/server.ex
@@ -1,22 +1,22 @@
 defmodule ElixirAmqpConsumer.Server do
   use GenServer.Behaviour
 
-  # defrecord AMQP.Msg,
+  # defrecord :amqp_msg,
   #           Record.extract(:amqp_msg, from: "./deps/amqp_client/include/amqp_client.hrl")
 
   # for now, must stub out any attribs that use fun in them
   # https://github.com/elixir-lang/elixir/issues/554
   # https://github.com/elixir-lang/elixir/issues/1242
-  defrecord AMQP.NetworkParams,
+  defrecord :ampq_params_network,
             Keyword.put(Record.extract(:amqp_params_network, from_lib: "amqp_client/include/amqp_client.hrl"),
                         :auth_mechanisms,
                         nil), as: :amqp_network_params
 
-  defrecord AMQP.ParamsDirect,
+  defrecord :amqp_params_direct,
             Record.extract(:amqp_params_direct, from: "./deps/amqp_client/include/amqp_client.hrl")
             # , as: :amqp_params_direct
 
-  defrecord AMQP.AdapterInfo,
+  defrecord :amqp_adapter_info,
             Record.extract(:amqp_adapter_info, from: "./deps/amqp_client/include/amqp_client.hrl"), as: :amqp_adapter_info
 
   def init(current_number) do
@@ -24,15 +24,16 @@ defmodule ElixirAmqpConsumer.Server do
     try do
 
       IO.puts "PARAMS"
-      params = AMQP.NetworkParams.new(password: "guest")
+      params = :amqp_params_direct.new(password: 'guest')
 
       IO.puts "CONNECTION"
       { :ok, connection } = :amqp_connection.start(params)
 
-#       inspect connection, raw: true
+      # IO.puts "CHANNEL"
+      # { :ok, channel} = :amqp_connection.open_channel(connection)
 
-#       IO.puts "CHANNEL"
-#       { :ok, channel} = :amqp_connection.open_channel(connection)
+
+
 #       # :amqp_channel.call(channel, #'queue.declare'{queue = <<"hello">>}),
 #
 #       # amqp_channel:cast(Channel,
